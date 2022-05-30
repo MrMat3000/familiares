e53231b9e75f3ab6a466941f86cc91408407ee6b
mateo ingouville <mateoingouville@gmail.com>
2022-05-30 00:04:26 -0300

    a ver si lo arreglé

--- familiares/models.py
+++ familiares/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Familiares(models.Model):
     name = models.CharField(max_length=40)
     age = models.IntegerField()
-
+    fecha = models.CharField(max_length=40)
